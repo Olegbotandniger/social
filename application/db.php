12173abd3aa63e830fd250a2b6b194a2e437dd5e
Olegbotandniger <122714019+Olegbotandniger@users.noreply.github.com>
2024-04-05 19:37:23 +0500

    Add files via upload

--- application/db.php
+++ application/db.php
@@ -7,5 +7,5 @@ $dbname = "social";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Проверка подключения
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,12 +11,14 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/menu.css">
 </head>
+
 <body>
     <?php
     include("menu.php");
-   ?>
+    ?>
     <div class="section-center">
         <h1 class="mb-0">Социальная сето с 0</h1>
     </div>
 </body>
+
 </html>
